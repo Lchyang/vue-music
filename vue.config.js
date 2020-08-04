9ba5266f1c39d41e980624afb109356569e60a1d
lichy <lichunyangcoder@gmail.com>
2020-08-04 15:46:04 +0800

    m-header develope

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,6 @@ module.exports = {
   chainWebpack: (config) => {
     config.resolve.alias
       .set('common', path.join(__dirname, './src/common'))
+      .set('components', path.join(__dirname, './src/components'))
   }
 }
