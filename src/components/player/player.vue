49eefd5862474258e0474d2e1c5d318778aef54c
lichy <lichunyangcoder@gmail.com>
2020-09-26 00:37:33 +0800

    add player animations

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -1,65 +1,78 @@
 <template>
   <div class="player" v-show="playList.length > 0">
-    <div class="full-screen" v-show="fullScreen">
-      <div class="background">
-        <img width="100%" height="100%" :src="currentSong.image" />
-      </div>
-      <div class="top">
-        <div class="back" @click="back">
-          <i class="icon-back"></i>
+    <transition
+      name="normal"
+      @enter="enter"
+      @after-enter="afterEnter"
+      @leave="leave"
+      @after-leave="afterLeave"
+    >
+      <div class="full-screen" v-show="fullScreen">
+        <div class="background">
+          <img width="100%" height="100%" :src="currentSong.image" />
         </div>
-        <h1 class="title">{{ currentSong.name }}</h1>
-        <h2 class="subtitle">{{ currentSong.singer }}</h2>
-      </div>
-      <div class="middle">
-        <div class="middle-left">
-          <div class="cd-wrapper">
-            <div class="cd">
-              <img class="image" :src="currentSong.image" />
+        <div class="top">
+          <div class="back" @click="back">
+            <i class="icon-back"></i>
+          </div>
+          <h1 class="title">{{ currentSong.name }}</h1>
+          <h2 class="subtitle">{{ currentSong.singer }}</h2>
+        </div>
+        <div class="middle">
+          <div class="middle-left">
+            <div class="cd-wrapper" ref="cdWrapper">
+              <div class="cd">
+                <img class="image" :src="currentSong.image" />
+              </div>
+            </div>
+          </div>
+          <div class="middle-right"></div>
+        </div>
+        <div class="bottom">
+          <div class="operators">
+            <div class="icon i-left">
+              <i class="icon-sequence"></i>
+            </div>
+            <div class="icon i-left">
+              <i class="icon-prev"></i>
+            </div>
+            <div class="icon i-center">
+              <i class="icon-play"></i>
+            </div>
+            <div class="icon i-right">
+              <i class="icon-next"></i>
+            </div>
+            <div class="icon i-right">
+              <i class="icon icon-not-favorite"></i>
             </div>
           </div>
         </div>
-        <div class="middle-right"></div>
       </div>
-      <div class="bottom">
-        <div class="operators">
-          <div class="icon i-left">
-            <i class="icon-sequence"></i>
-          </div>
-          <div class="icon i-left">
-            <i class="icon-prev"></i>
-          </div>
-          <div class="icon i-center">
-            <i class="icon-play"></i>
-          </div>
-          <div class="icon i-right">
-            <i class="icon-next"></i>
-          </div>
-          <div class="icon i-right">
-            <i class="icon icon-not-favorite"></i>
-          </div>
+    </transition>
+    <transition name="mini">
+      <div class="mini" v-show="!fullScreen" @click="handleFullScreen">
+        <div class="icon">
+          <img width="40px" heigth="40px" :src="currentSong.image" />
+        </div>
+        <div class="text">
+          <h1>{{ currentSong.name }}</h1>
+          <h2>{{ currentSong.singer }}</h2>
+        </div>
+        <div class="control">
+          <i class="icon-play-mini"></i>
+        </div>
+        <div class="control">
+          <i class="icon-playlist"></i>
         </div>
       </div>
-    </div>
-    <div class="mini" v-show="!fullScreen" @click="handleFullScreen">
-      <div class="icon">
-        <img width="40px" heigth="40px" :src="currentSong.image" />
-      </div>
-      <div class="text">
-        <h1>{{ currentSong.name }}</h1>
-        <h2>{{ currentSong.singer }}</h2>
-      </div>
-      <div class="control">
-        <i class="icon-play-mini"></i>
-      </div>
-      <div class="control">
-        <i class="icon-playlist"></i>
-      </div>
-    </div>
+    </transition>
   </div>
 </template>
 <script type="text/ecmascript-6">
 import { mapGetters, mapMutations } from 'vuex'
+import animations from 'create-keyframe-animation'
+import { prefixStyle } from 'common/js/dom'
+const transform = prefixStyle('transform')
 export default {
   computed: {
     ...mapGetters([
@@ -79,7 +92,70 @@ export default {
     },
     handleFullScreen () {
       this.setFullScreen(true)
+    },
+    enter (el, done) {
+      const { x, y, scale } = this._getPosAndScale()
+
+      const animation = {
+        0: {
+          transform: `translate3d(${x}px,${y}px,0) scale(${scale})`
+        },
+        60: {
+          transform: 'translate3d(0,0,0) scale(1.1)'
+        },
+        100: {
+          transform: 'translate3d(0,0,0) scale(1)'
+        }
+      }
+      animations.registerAnimation({
+        name: 'move',
+        animation,
+        presets: {
+          duration: 600,
+          easing: 'linear'
+        }
+      })
+      animations.runAnimation(this.$refs.cdWrapper, 'move', done)
+    },
+    afterEnter () {
+      animations.unregisterAnimation('move')
+      this.$refs.cdWrapper.style.animation = ''
+    },
+    leave (el, done) {
+      // 利用js绑定dom 实现动画
+      this.$refs.cdWrapper.style.transition = 'all 0.4s'
+      const { x, y, scale } = this._getPosAndScale()
+      this.$refs.cdWrapper.style[transform] = `translate3d(${x}px,${y}px,0) scale(${scale})`
+      const timer = setTimeout(done, 400)
+      this.$refs.cdWrapper.addEventListener('transitionend', () => {
+        clearTimeout(timer)
+        done()
+      })
+    },
+    afterLeave () {
+      this.$refs.cdWrapper.style.transition = ''
+      this.$refs.cdWrapper.style[transform] = ''
+    },
+    _getPosAndScale () {
+      // 底部mini图像的宽度
+      const targetWidth = 40
+      // mini图片中心离左边框的距离
+      const paddingLeft = 40
+      // mini图片中心离底边框的距离
+      const paddingBottom = 30
+      // cd上边框离顶部边框的距离
+      const paddingTop = 80
+      // cd 宽度
+      const width = window.innerWidth * 0.8
+      // 两个元素得比例
+      const scale = targetWidth / width
+      // x轴偏移量
+      const x = -(window.innerWidth / 2 - paddingLeft)
+      // y轴偏移量
+      const y = window.innerHeight - paddingTop - width / 2 - paddingBottom
+      return { x, y, scale }
     }
+
   }
 
 }
@@ -131,53 +207,65 @@ export default {
         color: $color-text
         font-size: $font-size-medium
         text-align: center
-  .middle
-    position: fixed
-    top: 80px
-    width: 100%
-    bottom: 170px
-    // white-space: nowrap
-    .middle-left
-      position: relative
+    .middle
+      position: fixed
+      top: 80px
       width: 100%
-      height: 0
-      padding-top: 80%
-      .cd-wrapper
-        position: absolute
-        top: 0
-        left: 10%
-        width: 80%
-        height: 100%
-        .image
+      bottom: 170px
+      // white-space: nowrap
+      .middle-left
+        position: relative
+        width: 100%
+        height: 0
+        padding-top: 80%
+        .cd-wrapper
           position: absolute
-          left: 0
           top: 0
-          width: 100%
+          left: 10%
+          width: 80%
           height: 100%
-          box-sizing: border-box
-          border-radius: 50%
-          border: 10px solid rgba(255, 255, 255, 0.1)
-  .bottom
-    position: absolute
-    bottom: 50px
-    width: 100%
-    .operators
-      display: flex
-      align-items: center
-      .icon
-        flex: 1
-        color: $color-theme
-        i
-          font-size: 30px
-      .i-left
-        text-align: right
-      .i-center
-        text-align: center
-        margin: 0 20px
-        .icon-play
-          font-size: 40px
-      .i-right
-        text-align: left
+          .image
+            position: absolute
+            left: 0
+            top: 0
+            width: 100%
+            height: 100%
+            box-sizing: border-box
+            border-radius: 50%
+            border: 10px solid rgba(255, 255, 255, 0.1)
+    .bottom
+      position: absolute
+      bottom: 50px
+      width: 100%
+      .operators
+        display: flex
+        align-items: center
+        .icon
+          flex: 1
+          color: $color-theme
+          i
+            font-size: 30px
+        .i-left
+          text-align: right
+        .i-center
+          text-align: center
+          margin: 0 20px
+          .icon-play
+            font-size: 40px
+        .i-right
+          text-align: left
+  // TODO: 动画仔细看一下
+  .normal-enter-active, .normal-leave-active
+    transition: all 0.4s
+    .top, .bottom
+      // 贝塞尔曲线 控制动画效果
+      transition: all 0.4s cubic-bezier(0.68, -0.55, 0.27, 1.55)
+  .normal-enter, .normal-leave-to
+    opacity: 0
+    .top
+      transform: translate3d(0, -100px, 0)
+    .bottom
+      transform: translate3d(0, 100px, 0)
   .mini
     display: flex
     align-items: center
@@ -221,4 +309,9 @@ export default {
         position: absolute
         left: 0
         top: 0
+  .mini-enter-active, .mini-leave-active
+    transition: all 0.5s
+  .mini-enter, .mini-leave-to
+    opacity: 0
+    transform: translate3d(0, 100px, 0)
 </style>
